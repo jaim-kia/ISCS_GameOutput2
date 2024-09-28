cac73b6d2b2837c0cde15b34261816fe7047370e
Jaim <131182310+jaim-kia@users.noreply.github.com>
2024-09-28 19:19:13 +0800

    Required Movement and Collission
    + moving platform and coin mechanic

--- GameOutput2/scripts/player.gd
+++ GameOutput2/scripts/player.gd
@@ -1,4 +1,5 @@
 extends CharacterBody2D
+@onready var animated_sprite_2d = $AnimatedSprite2D
 
 
 const SPEED = 130.0
@@ -14,15 +15,33 @@ func _physics_process(delta):
 		velocity.y += gravity * delta
 
 	# Handle jump.
-	if Input.is_action_just_pressed("ui_accept") and is_on_floor():
+	if Input.is_action_just_pressed("JumpMovement") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
 
-	# Get the input direction and handle the movement/deceleration.
-	# As good practice, you should replace UI actions with custom gameplay actions.
-	var direction = Input.get_axis("ui_left", "ui_right")
+
+	var direction = Input.get_axis("LeftMovement", "RightMovement")
+	
+	# To flip sprite based on direction
+	if direction == 1:
+		animated_sprite_2d.set_flip_h(0)
+	elif direction == -1:
+		animated_sprite_2d.set_flip_h(1)
+	
+	# Animation	
+	if is_on_floor():
+		if direction == 0:
+			animated_sprite_2d.play("idle")
+		else:
+			animated_sprite_2d.play("run")
+	else:
+		animated_sprite_2d.play("jump")
+	
+	# Movement
 	if direction:
 		velocity.x = direction * SPEED
 	else:
 		velocity.x = move_toward(velocity.x, 0, SPEED)
 
+
+		
 	move_and_slide()
